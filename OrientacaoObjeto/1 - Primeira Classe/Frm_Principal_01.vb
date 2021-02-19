58c46e18c89ea1b6a0012640ea38e8bd90cf9f2a
Franciel Leandro <francielleandro@gmail.com>
2021-02-19 07:04:20 -0300

    Criando tela inicial do projeto

---
+++ OrientacaoObjeto/1 - Primeira Classe/Frm_Principal_01.vb
@@ -0,0 +1,15 @@
+﻿Public Class Frm_Principal_01
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Me.Text = "1 - Primeira Classe"
+
+        Lbl_Principal.Text = "1 - Primeira Classe"
+
+    End Sub
+
+End Class
